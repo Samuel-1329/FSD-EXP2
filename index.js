ffe4155af7ea22e76c59800fde73933ec43280f3
Samuel-1329 <katisamuel4424@gmail.com>
2026-07-14 10:50:53 +0530

    Program-b

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser'); 
 
 const app = express();
 const PORT = 3000;
 
-// Set EJS as the templating engine
+// Set EJS as the templating engine 
 app.set('view engine', 'ejs');
- 
-// Route
-app.get('/', (req, res) => {
-    const user = {
-        name: 'SAMUEL',
-        course: 'Full Stack Development',
-        college: 'PSCMR College'
-    };
 
-    res.render('index', { user: user });
+// Middleware to parse form data 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Route to display form data
+app.get('/', (req, res) => {
+	res.render('index'); 
 });
 
-// Start server
+// Route to handle form submission 
+app.post('/submit', (req, res) => { 
+	// const name = req.body.name;
+	// const email = req.body.email;
+	const { name, email } = req.body; 
+	res.render('result', { name: name, email: email }); 
+});
+
+// Start server 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+	console.log(`Server running at http://localhost:${PORT}`); 
 });
